d4c00ec4f4212130a6fa0384c8609d499b0a31e2
Arun Kumar D <arun89@terpmail.umd.edu>
2018-11-20 01:03:41 -0500

    Added the main.cpp file for implementation of the walker node in gazebo. Converted tabs to spaces in both main.cpp and walker.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /************************************************************************************************
-* @file      : Implementation file for Week 12 turtlebot walker exercise
+* @file      : main file for Week 12 turtlebot walker exercise
 * @author    : Arun Kumar Devarajulu
-* @date      : November 19, 2018
+* @date      : November 20, 2018
 * @copyright : 2018, Arun Kumar Devarajulu
 * @license   : MIT License
 *
@@ -23,7 +23,7 @@
 *              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 *              SOFTWARE.
 *
-* @brief     : The walker.cpp contains the definitions for walker.h class methods
+* @brief     : The main.cpp contains the implementation walker node and laser scan subscription
 **************************************************************************************************/
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -31,17 +31,39 @@
 #include "geometry_msgs/Twist.h"
 #include "walker.h"
 
-void Walker::rangeCallback(const sensor_msgs::LaserScan::ConstPtr& scanInfo) {
-    float minimum = 10;
-    for (auto& item : scanInfo->ranges) {
-        if (minimum > distance) {
-            minimum = distance;
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "walker");
+    ros::NodeHandle n;
+    ros::Duration(5).sleep();
+    Walker turtle;
+    ros::Subscriber scanData;
+    ros::Publisher pub;
+    geometry_msgs::Twist msg;
+    // Subscribe to the laser scan topic using our callback function
+    scanData = n.subscribe("/scan", 1000, &Walker::rangeCallback, &turtle);
+    pub = n.advertise < geometry_msgs::Twist
+          > ("/mobile_base/commands/velocity", 100);
+    ros::Rate loop_rate(2);
+    while (n.ok()) {
+        // Initially all the linear and angular velocities are set as zero
+        msg.angular.x = 0.0;
+        msg.linear.x = 0.0;
+        msg.angular.y = 0.0;
+        msg.linear.y = 0.0;
+        msg.angular.z = 0.0;
+        msg.linear.z = 0.0;
+        if (turtle.findObstacleRange() > .9) {
+            // When there is no obstacle, we move forward
+            msg.linear.x = 0.15;
+            ROS_INFO_STREAM("Heading in forward direction");
+        } else {
+            // When we encounter an obstacle, we make a manuevre
+            msg.angular.z = -0.5;
+            ROS_WARN_STREAM("Obstacle found ahead."
+                            " Making an avoidance manuevre");
         }
+        pub.publish(msg);
+        ros::spinOnce();
+        loop_rate.sleep();
     }
-    ObstacleRange = minimum;
-    ROS_INFO_STREAM("Obstacle found in : " << ObstacleRange << " units range!");
-}
-
-float Walker::findObstacleRange() {
-    return ObstacleRange;
 }
